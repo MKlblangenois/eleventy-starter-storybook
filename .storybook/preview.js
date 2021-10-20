f42f6fd3cc145e1248330a88c0e4015d540d0cb8
Logan Blangenois <logan.blangenois@mediakod.com>
2021-10-20 23:02:46 +0200

    chore: add css to SB

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
+import "../_site/static/assets/styles/main.min.css";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+   actions: { argTypesRegex: "^on[A-Z].*" },
+   controls: {
+      matchers: {
+         color: /(background|color)$/i,
+         date: /Date$/,
+      },
+   },
+};
